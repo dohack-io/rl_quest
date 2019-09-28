80f3a5b383c8531b0195e21d88ecf5cc3feb1b4b
c0demonk <c0demonk@github.com>
2019-09-28 14:58:59 +0200

    Basic Login page

--- rl_quest/src/App.js
+++ rl_quest/src/App.js
@@ -1,13 +1,54 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import GoogleLogin from 'react-google-login'
+import Box from '@material-ui/core/Box'
+import Button from '@material-ui/core/Button'
 
-function GoogleSignin() {
-  return <div class="g-signin2" data-onsuccess=""></div>
+class LoginPage extends React.Component {
+  constructor(props)
+  {
+    super(props)
+  }
+  
+  dummyLogin = () => {
+    this.props.loginFinished()
+  };
+  
+  responseGoogle = () => {
+    // TODO this.props.loginFinished()
+  };
+  
+  render() {
+    return <Box className="LoginBox">
+        <h3>Login</h3>
+        <GoogleLogin
+          clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+          buttonText="Login with Google"
+          onSuccess={this.responseGoogle}
+          onFailure={this.responseGoogle}
+          cookiePolicy={'single_host_origin'}
+        />
+        <Button color="primary" variant="contained" onClick={this.dummyLogin} >Dummy Login</Button>
+      </Box>;
+  }
 }
 
-function App() {
-  return <div>Hello World</div>// <GoogleSignin />;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { loggedIn: false }
+  }
+  
+  finish = () => this.setState({ loggedIn: true })
+  
+  render() {
+    if(!this.state.loggedIn)
+      return <LoginPage loginFinished={this.finish} />
+    else
+      return <div>Hello, World</div>
+  }
 }
 
 export default App;
