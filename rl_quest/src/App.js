a11c442d38f402d54ff4919a0fbd13956eb93d64
c0demonk <c0demonk@github.com>
2019-09-28 13:03:48 +0200

    Created react project...

---
+++ rl_quest/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function GoogleSignin() {
+  return <div class="g-signin2" data-onsuccess=""></div>
+}
+
+function App() {
+  return <div>Hello World</div>// <GoogleSignin />;
+}
+
+export default App;
